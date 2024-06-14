01ccd570ccf1c3fdea56e0511c8daa46e11541ce
Ikenna Agu <ike.john.agu@gmail.com>
2024-06-14 01:58:51 -0700

    one more

--- 0x00-shell_basics/0-current_working_directory.sh
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -1,10 +1,7 @@
-#/bin/sh
-echo "${0}"     # full script name
-echo "${0##*/}" # script name
-echo "${0%/*}"  # script path
-echo "$PWD"     # current working directory
+#/bin/bash
+
 echo "$(pwd)"   # current working directory
-pwd             # current working directory
+
 
 
 
