f1cbeaebd50a1f6a46718459f9389517a9f74b0a
Omnilutiondev <ike.john.agu@gmail.com>
2025-01-20 17:37:06 +0100

    SSH - task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 # This script will setup my client config file using puppet
-
-file {'/etc/ssh/ssh_config':
-  ensure => 'present',
+file {'/etc/ssh/shh_config':
+	ensure  => 'present',
 }
 
 file_line {'Turn off passwd auth':
+	path    => '/etc/ssh/ssh_config',
+	line    => 'PasswordAuthentication no',
+	match   => 'PasswordAuthentication yes',
+	replace => 'true',
+	
 }
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  match  => 'PasswordAuthentication yes',
-  replace => 'true',
-
-file_line {'Delare identity file':
-  ensure => 'present'
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^IdentityFile',
+file_line {'Declare identity file':
+	path    => '/etc/ssh/ssh_config',
+	line    => 'IdentityFile ~/.ssh/school',
+	match   => '^IdentityFile',
+	ensure  => 'present',
 }
