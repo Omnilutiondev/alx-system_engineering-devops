d6dbc1ebaf5f067dad1e8d5777846ebee0d5c772
Omnilutiondev <ike.john.agu@gmail.com>
2025-02-25 06:58:22 +0100

    0x15-api - Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
 """This script will return the to-do list info for an employee ID."""
 import requests
-import sys
+from sys import argv
+""" and then run a request module used to fetch the url response"""
+
+
+def get_employee_todo_progress(employee_id):
+    """this is the  employee to do progress function"""
+    try:
+        url = "https://jsonplaceholder.typicode.com/"
+        user_datas = requests.get(url + f"users/{employee_id}")
+        user_data = user_datas.json()
+        employee_name = user_data['name']
+
+        """run a fetch for  the todos list for an employee
+        using todos?<dic_key>=value"""
+        todos_list = requests.get(url + f"todos?userId={employee_id}")
+        json_todos_list = todos_list.json()
+
+        """calculate the task done and left undone"""
+        total_task = len(json_todos_list)
+        """check the completed tasks"""
+        task_done = [task for task in json_todos_list if task['completed']]
+        no_task_done = len(task_done)
+
+        """display all results"""
+        print(f"Employee {employee_name} is done with tasks("
+              f"{no_task_done}/{total_task}):")
+        """printing the title of completed task"""
+        for task in task_done:
+            print(f"\t {task['title']}")
+    except Exception as e:
+        print(f"an error occured: {e}")
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    if len(argv) != 2:
+        print("Usage: script <emplyee_id>")
+    else:
+        get_employee_todo_progress(argv[1])
